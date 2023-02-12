1cbd2184370c2ce9e4c53828d6b704a4cf6afb1e
GitHub Action <action@github.com>
2023-02-12 06:16:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/a524631266/owgeneratorcode/MyBundle.kt
+++ src/main/kotlin/com/github/a524631266/owgeneratorcode/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.a524631266.owgeneratorcode
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/a524631266/owgeneratorcode/services/MyProjectService.kt
+++ src/main/kotlin/com/github/a524631266/owgeneratorcode/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.a524631266.owgeneratorcode.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.a524631266.owgeneratorcode.MyBundle
 
 class MyProjectService(project: Project) {
 
