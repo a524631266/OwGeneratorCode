17099fbb03423a03a918f4f6c88fe3a2c9a235d6
张亮亮 <liangliang.zhang4@geely.com>
2023-02-23 00:05:07 +0800

    [add]: 删除初始化配置 1、javax 2、javafx

--- src/main/java/com/github/a524631266/owgeneratorcode/ProjectViewPopupMenuDemo.java
+++ src/main/java/com/github/a524631266/owgeneratorcode/ProjectViewPopupMenuDemo.java
@@ -1,9 +1,6 @@
 package com.github.a524631266.owgeneratorcode;
 
 import com.github.a524631266.owgeneratorcode.action.ActionUtils;
-import com.github.a524631266.owgeneratorcode.ui.bootstrap.fx.Sampler;
-import com.github.a524631266.owgeneratorcode.ui.bootstrap.fx.ShowConfigUI;
-import com.github.a524631266.owgeneratorcode.ui.java.fx.TestCaseConfigUI;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.Project;
@@ -18,7 +15,6 @@ public class ProjectViewPopupMenuDemo extends AnAction {
         // 使用javax制作ui
 //        new TestCaseConfigUI().showUI(project);
         // 使用javafx制作ui
-        new Sampler(project);
-        Sampler.launch();
+//        Application.launch(Sampler.class, "aa");
     }
 }
--- src/main/java/com/github/a524631266/owgeneratorcode/ui/bootstrap/fx/ShowConfigUI.java
+++
@@ -1,8 +0,0 @@
-package com.github.a524631266.owgeneratorcode.ui.bootstrap.fx;
-
-public class ShowConfigUI {
-
-    public static void main(String[] args) {
-
-    }
-}
